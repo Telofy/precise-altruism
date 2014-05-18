4c1b5b07c3a6ae400200fd916c4b0294f7173ece
buriti <buriti@buriti.(none)>
2014-05-18 18:05:12 +0200

    in Arbeit: Versuch eines Pythonskripts

--- corpuscreator_attempt.py
+++ corpuscreator_attempt.py
@@ -10,7 +10,7 @@ RAW_FILES = ('altruism.2014-05-13.json',
 
 def select(dictionary, keys):
     dict_ = copy(dictionary)
-    for key in keys
+    for key in keys:
         dict_ = dict_.get(key, {})
     return key, dict_
 
@@ -27,10 +27,15 @@ def run():
     for raw_file in RAW_FILES:
         with open(raw_file) as json_file:
             data.extend(json.load(json_file)['hits']['hits'])
+    categorization = {}
     for hit in data:
+	hit_data = {}
 	for keys in args.key:
-	    key, value = select(hit['_source'], key.split('.'))
-	
-
+	    the_key, value = select(hit['_source'], keys.split('.'))
+	    hit_data[the_key] = value
+	print hit_data['body']
+	print hit_data['title']
+	cat_ = raw_input("0 or 1? ")
+	categorization[hit_data.get('link_url')] = int(cat_)
 if __name__ == '__main__':
     run()
