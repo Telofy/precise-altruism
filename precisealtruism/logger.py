ad22c565e03374ded405d56901f4b0e8b361bb47
Denis Drescher <drescher@ferret-go.com>
2016-05-24 10:44:07 +0200

    Logging fix

--- precisealtruism/logger.py
+++ precisealtruism/logger.py
@@ -3,12 +3,9 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 import logging
 
-NAME = 'precisealtruism'
-LEVEL = logging.INFO
-
-logger = logging.getLogger(NAME)
-logger.setLevel(LEVEL)
-logger.root.handlers[0].setFormatter(logging.Formatter(
-    '%(asctime)s: %(levelname)s: %(funcName)s (%(thread)d): %(message)s'))
-
-logger.root.setLevel(logging.WARNING)  # Readability fix
+logging.basicConfig(
+    format='%(asctime)s: %(levelname)s:'
+           ' %(funcName)s (%(thread)d):'
+           ' %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
